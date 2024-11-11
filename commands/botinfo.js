976eaac7011b13890d231b90e72b5634ceb646d8
Tiiker1 <59542174+Tiiker1@users.noreply.github.com>
2024-11-11 08:35:56 +0200

    Create botinfo.js
    Intial Commit for old botinfo command

---
+++ commands/botinfo.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('botinfo')
+    .setDescription('Displays information about the bot'),
+  
+  async execute(interaction) {
+    const { client } = interaction;
+    await interaction.reply(`Bot username: ${client.user.username}\nID: ${client.user.id}\nServers: ${client.guilds.cache.size}\nUptime: ${Math.round(client.uptime / 1000)} seconds\nVersion:0.1`);
+  },
+};
