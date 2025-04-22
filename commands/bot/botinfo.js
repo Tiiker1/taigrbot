111ddd19177cae723c9f9d687a662028e140ee99
Tiiker1 <59542174+Tiiker1@users.noreply.github.com>
2025-04-22 19:39:52 +0300

    Update botinfo.js
    updated bot version

--- commands/bot/botinfo.js
+++ commands/bot/botinfo.js
@@ -7,6 +7,6 @@ module.exports = {
   
   async execute(interaction) {
     const { client } = interaction;
-    await interaction.reply(`Bot username: ${client.user.username}\nID: ${client.user.id}\nServers: ${client.guilds.cache.size}\nUptime: ${Math.round(client.uptime / 1000)} seconds\nVersion:0.1`);
+    await interaction.reply(`Bot username: ${client.user.username}\nID: ${client.user.id}\nServers: ${client.guilds.cache.size}\nUptime: ${Math.round(client.uptime / 1000)} seconds\nVersion:1.0.0`);
   },
 };
